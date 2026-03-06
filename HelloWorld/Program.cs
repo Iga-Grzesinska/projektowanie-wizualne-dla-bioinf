f62af582708d9141c819622c685414f3483458b2
Iga Grzesińska <iga.notofficial@laptop-Iga.local>
2026-03-06 08:13:25 +0100

    dodano test działania kalkulatora, usunięto pliki bin/ i obj/ z repo

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,5 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Pierwszy tekst");
-Console.WriteLine("Drugi tekst");
-Console.WriteLine("Trzeci tekst");
+using System;
+using CalculatorLibrary;
+
+class Program
+{
+    static void Main()
+    {
+	Console.WriteLine("Hello, World!");
+	Console.WriteLine("Pierwszy tekst");
+	Console.WriteLine("Drugi tekst");
+	Console.WriteLine("Trzeci tekst");
+	Console.WriteLine("Test kalkulatora:");
+
+        Calculator calc = new Calculator();
+
+        Console.WriteLine("2 + 3 = " + calc.Add(2,3));
+        Console.WriteLine("5 - 2 = " + calc.Subtract(5,2));
+        Console.WriteLine("4 * 3 = " + calc.Multiply(4,3));
+        Console.WriteLine("10 / 2 = " + calc.Divide(10,2));
+    }
+}
