b0ed9b345ea41351826df94599fe93999879c10c
Iga Grzesińska <iga.notofficial@laptop-iga.home>
2026-03-27 19:56:51 +0100

    Dodano wczytywanie obrazu

--- ImageApp/MainWindow.axaml.cs
+++ ImageApp/MainWindow.axaml.cs
@@ -1,4 +1,7 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Media.Imaging;
+using System.IO;
 
 namespace ImageApp;
 
@@ -7,5 +10,28 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private async void LoadImage_Click(object sender, RoutedEventArgs e)
+    {
+        var dialog = new OpenFileDialog();
+        dialog.Title = "Wybierz obraz";
+        dialog.Filters.Add(new FileDialogFilter()
+        {
+            Name = "Obrazy",
+            Extensions = { "bmp", "png", "jpg" }
+        });
+
+        var result = await dialog.ShowAsync(this);
+
+        if (result != null && result.Length > 0)
+        {
+            var filePath = result[0];
+
+            using (var stream = File.OpenRead(filePath))
+            {
+                MainImage.Source = new Bitmap(stream);
+            }
+        }
     }
 }
